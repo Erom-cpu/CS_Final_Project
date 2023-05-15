e6d73c2fe4a95baf4618de5a4c79556b1da8ac10
Rom Erez <rerez2025@kehillahstudent.org>
2023-05-15 12:18:54 -0700

    MAY 16

--- final_project.py
+++ final_project.py
@@ -3,39 +3,26 @@ import bs4 #this allows to call the data in different ways
 import pandas as pd #turns link to html
 import matplotlib.pyplot as plt #plots the data
 from itertools import cycle, islice
-import pandas, numpy as np  # I find np.random.randint to be better
+import pandas, numpy as np  #p.random.randint sometimes better
 
 
-url = 'https://en.wikipedia.org/wiki/List_of_states_with_nuclear_weapons'
-# response = requests.get(url)
-# soup = bs4.BeautifulSoup(response.text)
-
-# pretty_html_string = soup.prettify()
-
-# with open("nuclear_wiki.htm", "w") as f:
-#     f.write(pretty_html_string)
-
-# df = pd.read_csv("nuclear_wiki.htm")
-# print(df.columns)
+url = 'https://en.wikipedia.org/wiki/List_of_states_with_nuclear_weapons' #Website URL
 
 dfs = pd.read_html(
     url,
-    match = "Overview of nuclear states and their capacities"
+    match = "Overview of nuclear states and their capacities" #Graph's title in website
 )
 df = dfs[0]
+#following code solves astype() problem 
 df.columns = df.columns.to_flat_index()
 df[('Warheads[a]', 'Total')] = df[('Warheads[a]', 'Total')].str.replace(r"\[.*\]","")
 df[("Country", "Country")] = df[("Country", "Country")].str.replace(r"\[.*\]","")
 df = df[[("Country", "Country"),('Warheads[a]', 'Total')]].astype({('Warheads[a]', 'Total'): 'int32'})
 print(df)
 
-#Code for graph (Line graph (red))
-# df.plot(x = ('Country', 'Country'), y = ('Warheads[a]', 'Total'), lw =2, 
-# colormap = 'flag', marker = '.', markersize = 10, title = "Amount of nuclear warheads per state")
-
-#Code for graph (Bar graph (green))
+#Code for bar graph
 df.plot(x = ('Country', 'Country'), y = ('Warheads[a]', 'Total'), kind = 'bar', 
-colormap = 'Accent', title = "Amount of nuclear warheads per state")
+colormap = 'Accent', title = "Amount of nuclear warheads per state") #Adds graph color and graph title
 
 plt.show()
 print(df.info())
